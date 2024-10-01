9590b89cd07652999b88127081146c30fc756b1b
Quyen Vong <qvong97@gmail.com>
2024-10-01 15:33:23 -0400

    Paul's code

--- logic-and-math/src/main/java/com/pluralsight/logic/LogicExamples.java
+++ logic-and-math/src/main/java/com/pluralsight/logic/LogicExamples.java
@@ -3,77 +3,71 @@ package com.pluralsight.logic;
 public class LogicExamples {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello");
 
+        double area = 10 / 3;           //  integer division
+        double remainder = 10.0 % 3.0;  //  floating point modulo (remainder)
 
-        double area = 10 / 3;
-        double remainder = 10.0 % 3.0;
-
-        area = (double) 10 / 3;
+        area = (double) 10 / 3;         //  fp division
 
         System.out.println( area );
         System.out.println( remainder );
 
-        // Arithmetic Operators
-        // + - / * %
-        // Increment/decrement operators
-        // ++ --
-        //
+        // Arithmetic operators
+        //  +  -  /  *  %
+        // Increment/decrement operators - PREFIX and POSTFIX versions
+        //  ++  --
 
         area = area + 1;
-        System.out.println( area );
+        System.out.println(area);  // what value prints here?
 
-        area ++;
-        System.out.println( area );
+        area ++;    // postfix increment
+        System.out.println(area);
 
-        System.out.println(++ area); //6.33
+        System.out.println(++ area);  // what value prints here?
 
-        System.out.println(++ area + area ++ + remainder ++);
+        System.out.println(++ area + area ++ + remainder ++);  // what value prints here?
 
-        System.out.println(area); //8.33
-        System.out.println(remainder); //2
+        System.out.println(area);       // what value prints here?
+        System.out.println(remainder);  // what value prints here?
 
         float x = (float) remainder;
-        System.out.println(x); //2.0
+        System.out.println(x);   // what value prints here?
 
         float y = 7.99f;
-        System.out.println(y); //7.99
+        System.out.println(y);  // what value prints here?
 
         double z = 8.33f;
-        System.out.println(z);
+        System.out.println(z);  // what value prints here?
+
         System.out.printf("%.2f\n", z);
-        System.out.println(z);
+        System.out.println(z);  // what value prints here?
 
 
-        // Comparison Operators
-        // == != < > >= <= !
+        // Comparison operators
+        // ==  !=  <  >  >=  <=  !
 
         boolean done = false;
         int counter = 0;
         do {
-            System.out.println("talking...words...");
-            counter ++;
+            System.out.println("talking.... words...");
             if ( counter++ == 5 ) done = true;
-        } while ( ! done);
+        } while( ! done);
         System.out.println(counter);
 
         counter =0;
-        while(counter !=5){
-            System.out.println("talking...words...");
+        while(counter != 5){
+            System.out.println("talking.... words...");
             counter ++;
-        }
-
-        for (int i=0; i<5; i++);{
-            System.out.println("Talking...");
         }
 
         for (int i=0; i<5; i++){
             System.out.println("Talking...");
         }
 
-        if (counter >0) {
+
+        if (counter > 0) {
             System.out.println("We talked about it");
         }
-
-        }
     }
+}
